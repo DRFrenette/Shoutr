6b13e85619eef1c72671e1b0724ad34595ba5064
Dan Frenette <paradox815@gmail.com>
2015-01-28 22:41:23 -0500

    Create user show pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "shouts#index"
 
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :index] do
+  resources :users, only: [:new, :create, :index, :show] do
     resource :following_relationship, only: [:create, :destroy]
   end
   resources :shouts, only: [:index, :create]
